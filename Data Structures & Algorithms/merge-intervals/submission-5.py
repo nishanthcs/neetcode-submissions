dc4a8c22bc5a155cce388513fa863e651c3f7f4c
nishanthcs <nishanth.menon@gmail.com>
2026-06-23 21:13:06 -0400

    Add: merge-intervals - submission-5

---
+++ Data Structures & Algorithms/merge-intervals/submission-5.py
@@ -0,0 +1,26 @@
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # Sort in place
+        intervals.sort(key=lambda x: (x[0],x[1]))
+
+        # write represents the last merged index. 
+        # read is the value we compare it to.
+        write=0
+        read=1
+        start=0
+        end=1
+
+        while read < len(intervals):
+            if intervals[write][end] >= intervals[read][start]:
+                # overlap, merge read into write. 
+                intervals[write][end] = max(intervals[write][end], intervals[read][end])
+                read += 1
+            else:
+                # no overlap, increment write. 
+                write += 1
+                # overwrite the stale interval with this read.
+                intervals[write] = intervals[read]
+                read += 1
+
+        return intervals[:write+1]
+
